b699fa8fafde76e6556274516c64ffe84fe84676
arzeusy <jkob1994@gmail.com>
2023-01-20 15:46:43 -0600

    declaracion de variables, en el caso de go al declara un variable sin valor esta tomara una por defecto y no null como en otros lenguajes

--- src/main.go
+++ src/main.go
@@ -3,6 +3,25 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hola mundo!!!")
 
+	const pi float64 = 3.14
+	const pi2 = 3.14
+
+	fmt.Println(pi, pi2)
+
+	//declaracion de variables enteras
+	//
+
+	base := 12
+	var altura int = 12
+	var area int
+
+	fmt.Println(base, altura, area)
+
+	// zero values
+	var b float64
+	var c string
+	var d bool
+
+	fmt.Println(b, c, d)
 }
