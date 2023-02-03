5daf32504bcc8fa8dd263d677394913db735eeec
arzeusy <jkob1994@gmail.com>
2023-02-03 15:14:22 -0600

    funciones anonimas

--- src/main.go
+++ src/main.go
@@ -2,21 +2,31 @@ package main
 
 import "fmt"
 
-func main() {
-
-	hellomessage := "hello"
-	worldMessage := "world"
-
-	fmt.Println(hellomessage, worldMessage)
-	fmt.Println(hellomessage, worldMessage)
-
-	// %s para formatos string
-	// %d para formatos numericos
-	// %v para formatos dynamicos
-	fmt.Printf("%s tine mas de %v curso\n", hellomessage, worldMessage)
-
-	message := fmt.Sprintf("%s tine mas de %v curso", hellomessage, worldMessage)
-
+func normalFunction(message string) {
 	fmt.Println(message)
-	fmt.Printf("hellomessage: %T\n", hellomessage)
+
+}
+
+func tripleArgument(a, b int, c string) {
+	fmt.Println(a, b, c)
+}
+
+func returnValue(a int) int {
+	return a * 2
+}
+
+func returnNValue(a int) (c, d int) {
+	return a * 2, a
+}
+
+func main() {
+	normalFunction("Hola mundo")
+	tripleArgument(1, 2, "hola")
+
+	fmt.Println("value: ", returnValue(2))
+
+	// si no requiere una varaiable de lo retornado lo podemos especificar con "_" piso
+	// por ejemplo value1, _ := returnNValue(2)
+	value1, vlaue2 := returnNValue(2)
+	fmt.Println("value1: ", value1, " value2: ", vlaue2)
 }
